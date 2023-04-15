d449169fef0f4e29a55c767c819f8568df5c2953
Abner Lima <abnerlima.1997@gmail.com>
2023-04-15 19:19:47 -0300

    Configurando o projeto

---
+++ TrallerFix/TrailerViewController.swift
@@ -0,0 +1,28 @@
+//
+//  TrailerViewController.swift
+//  TrallerFix
+//
+//  Created by Abner Lima on 15/04/23.
+//
+
+import UIKit
+
+class TrailerViewController: UIViewController {
+
+    @IBOutlet weak var ivTrailer: UIImageView!
+    @IBOutlet weak var lbTitle: UILabel!
+    @IBOutlet weak var lbYear: UILabel!
+    @IBOutlet weak var lbRating: UILabel!
+    @IBOutlet weak var viTrailer: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func close(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+
+}
